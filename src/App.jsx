d32a83ad4ee14054698dd7801e7ab1c737c0970b
mcnultye <157040192+mcnultye@users.noreply.github.com>
2025-10-15 21:29:26 -0400

    adding navigation bar to the top of the screen

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import Header from "./components/Header.jsx";
 import Sidebar from "./components/Sidebar.jsx";
 import Dashboard from "./components/Dashboard.jsx";
 import Footer from "./components/Footer.jsx";
+import NavBar from "./components/NavBar.jsx";
 
 function App() {
   return (
     <div>
-      <Header />
-      <div style={{ display: "flex" }}>
+      <NavBar />
+      <div style={{ display: "flex", marginTop: "60px" }}>
         <Sidebar />
         <Dashboard />
       </div>
