bf5fbbd624fbf0dd1c5c6a1db7ba9994523e52b9
mcnultye <157040192+mcnultye@users.noreply.github.com>
2025-11-05 16:58:05 -0500

    Adding logo, search bar, and home icon to nav bar

---
+++ eaeinc/components/Header.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link"; // <-- correct Link
+import { HomeIcon, SearchIcon } from "lucide-react";
+
+function Header() {
+  return (
+    <div className="flex items-center p-2">
+      <Image src="/logo.jpg" alt="Logo" width={100} height={100} />
+
+      <div className="flex-1">
+        <form className="flex items-center space-x-1 bg-gray-100 p-2 rounded-md flex-1 mx-2 max-w-96">
+          <SearchIcon className="h-4 text-gray-600" />
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-transparent flex-1 outline-none"
+          />
+        </form>
+      </div>
+
+      <div>
+        <Link href="/">
+          <div className="flex flex-col items-center icon">
+            <HomeIcon className="h-5 w-5" />
+            <span className="text-sm mt-1">Home</span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
