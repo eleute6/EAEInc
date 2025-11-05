45dbbcc02b04ba9aadfa0f3d1d76f32a605b2c5e
mcnultye <157040192+mcnultye@users.noreply.github.com>
2025-11-05 16:57:17 -0500

    setup for actual page components

--- eaeinc/app/layout.tsx
+++ eaeinc/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,9 @@ export default function RootLayout({
       >
         {/* TOASTER */}
 
-        {/* Header */}
+        <header>
+          <Header />
+        </header>
 
         <div>
           <main>{children}</main>
--- eaeinc/app/page.tsx
+++ eaeinc/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
-    <div className="">
-      <main className="">
-        <h1>EAEInc Community Research Page</h1>
-      </main>
-    </div>
+    <main className="flex">
+      <h1>EAEInc Community Research Page</h1>
+    </main>
   );
 }
