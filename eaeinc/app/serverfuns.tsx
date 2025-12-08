af14753dd2359a77bc6e3e7c3da12eb87f8a84cd
ALang624 <langak@merrimack.edu>
2025-12-08 16:58:42 -0500

    Removed Unncessary Files, Added Actions File to Fill.

---
+++ eaeinc/app/serverfuns.tsx
@@ -0,0 +1,13 @@
+/* ACTIONS FOR PAGES TO USE */
+/*
+    DEVELOPER'S NOTE:
+    This page contains within all actions that can or should be used by the various pages
+    for this project. This is done for the ease of calling and for the sanity of any future
+    development and fixing. Each function will be explained below through comments, though
+    any questions should be researched in the technical document.
+*/
+'use server'
+
+export async function sendPost(fData: FormData) {
+    
+}
